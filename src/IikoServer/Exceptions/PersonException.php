6602989abd3854370b0828964b2787dabf7717bb
a.ivanov@bo.cafe <a.ivanov@bo.cafe>
2021-06-30 17:22:34 +0300

    Persons added

--- src/IikoServer/Exceptions/PersonException.php
+++ src/IikoServer/Exceptions/PersonException.php
@@ -4,7 +4,9 @@
 namespace IikoServer\Api\Exceptions;
 
 
-class PersonException
+class PersonException extends \Exception
 {
-
+    public static function requiredValueNotFound(string $message): self {
+        return new static($message);
+    }
 }
--- src/IikoServer/Methods/Persons.php
+++ src/IikoServer/Methods/Persons.php
@@ -4,9 +4,12 @@
 namespace IikoServer\Api\Methods;
 
 
+use DOMDocument;
+use IikoServer\Api\Exceptions\PersonException;
 use IikoServer\Api\IikoRequests;
 use IikoServer\Api\Objects\PersonObject;
 use SimpleXMLElement;
+use function Webmozart\Assert\Tests\StaticAnalysis\string;
 
 trait Persons
 {
@@ -15,6 +18,13 @@ trait Persons
 
     protected $persons = [];
 
+    private $personInfo = [
+        "firstname" => null,
+        "lastName" => null,
+        "code" => null,
+        "mainRoleCode" => null,
+        "cardNumber" => null
+    ];
     /**
      * Return array of persons organization
      * @param array $params
@@ -25,19 +35,54 @@ trait Persons
         $persons = new SimpleXMLElement($request);
         foreach ($persons->employee as $person){
             $this->persons[] = new PersonObject(
-                $person->id,
-                $person->code,
-                $person->name,
-                $person->login,
-                $person->mainRoleId,
-                $person->mainRoleCode,
-                $person->cardNumber,
-                $person->snils,
-                $person->deleted,
+                strval($person->id),
+                strval($person->code),
+                strval($person->name),
+                strval($person->login),
+                strval($person->mainRoleId),
+                strval($person->mainRoleCode),
+                strval($person->cardNumber),
+                strval($person->snils),
+                strval($person->deleted),
             );
         }
 
         return $this->persons;
 
+    }
+
+    /**
+     * @param array $params
+     * @return PersonObject
+     * @throws PersonException
+     */
+    private function addPerson(array $params) {
+        $this->validateFirstName($params);
+        $dom = new DomDocument('1.0');
+        $newPerson = $dom->appendChild($dom->createElement('employee'));
+
+        foreach ($this->personInfo as $key => $info){
+            $code = $newPerson->appendChild($dom->createElement($key));
+            $code->appendChild($dom->createTextNode($this->personInfo[$key]));
+        }
+
+        $dom->formatOutput = true;
+        $result = $dom->saveXML();
+
+        var_dump($result);
+    }
+
+    /**
+     * @param array $params
+     * @throws PersonException
+     */
+    public function validateFirstName(array $params){
+        foreach ($this->personInfo as $key => $info){
+            if (array_key_exists($key, $params) && is_string($params[$key])){
+                $this->personInfo[$key] = $params[$key];
+            } else {
+                throw PersonException::requiredValueNotFound('Required "'.$key.'" not supplied in parameters');
+            }
+        }
     }
 }
